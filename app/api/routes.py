8c5444cefcec038a90efd489cf0aa75f6cb206ec
Iran-AP-BR <jran.santos20@gmail.com>
2021-01-30 18:01:55 -0300

    definindo métodos permitidos para cada rota

--- app/api/routes.py
+++ app/api/routes.py
@@ -7,10 +7,9 @@ from app.api.resources import data_atual, files, municipios, swagger
 
 
 def init_routes(app):
-   app.add_url_rule('/', 'index', swagger)
-   #app.add_url_rule('/docs', 'docs', swagger)
-   app.add_url_rule('/data_atual', 'data_atual', data_atual)
-   app.add_url_rule('/municipios', 'municipios', municipios)
-   app.add_url_rule(f'/{app.config["DATA_ENDPOINT"]}/<tableName>', app.config["DATA_ENDPOINT"], files)
+   app.add_url_rule('/', 'index', swagger, methods=['GET'])
+   app.add_url_rule('/data_atual', 'data_atual', data_atual, methods=['GET'])
+   app.add_url_rule('/municipios', 'municipios', municipios, methods=['GET'])
+   app.add_url_rule(f'/{app.config["DATA_ENDPOINT"]}/<tableName>', app.config["DATA_ENDPOINT"], files, methods=['GET'])
 
    return True
